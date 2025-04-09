3d33973b87a14937ff8118e65ef67d72845661f2
SachinKumarMishra87 <sachinmishra8707@gmail.com>
2025-04-09 12:15:52 +0530

    server chanages

--- server/index.js
+++ server/index.js
@@ -7,10 +7,18 @@ dotenv.config()
 import router from "./routes/userRouter.js";
 import cookieParser from "cookie-parser";
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}))
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true
+// }))
+
+app.use(
+    cors({
+      origin: ["https://chat-app-real-time-cyan.vercel.app"], // Apne frontend ka actual URL yaha set karo
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
 
 app.use(express.json())
 const PORT = process.env.PORT || 8080
